280dff5da068f763e857029b470cf4a441a0dc05
Toni Kangas <toni.ar.kangas@gmail.com>
2017-12-25 16:28:41 +0200

    Use enzyme.

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,8 +1,9 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { mount } from 'enzyme';
 import App from '../App';
 
-it('renders without crashing', () => {
-	const div = document.createElement('div');
-	ReactDOM.render(<App />, div);
+describe('App',()=>{
+	it('renders without crashing', () => {
+		mount(<App />);
+	});
 });
