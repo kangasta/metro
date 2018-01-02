13d7fd7ce3db33d44c234238f09db1c903640c8d
Toni Kangas <toni.ar.kangas@gmail.com>
2018-01-03 01:07:18 +0200

    Check timers are cleared at unmount.

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,9 +1,21 @@
 import React from 'react';
-import { mount } from 'enzyme';
+import { mount, shallow } from 'enzyme';
 import App from '../App';
+
+jest.useFakeTimers();
 
 describe('App',()=>{
 	it('renders without crashing', () => {
 		mount(<App />);
+	});
+	it('clears timers at unmount', () => {
+		const wrapper = shallow(<App />);
+		const ids = [
+			wrapper.state('queryIntervalId'),
+			wrapper.state('coordsIntervalId')
+		];
+		wrapper.unmount();
+		for (var i = 0; i < ids.length; i++)
+			expect(clearInterval).toHaveBeenCalledWith(ids[i]);
 	});
 });
