a8afaacff5ad3fb169b82673c5cf3bc6674c1746
Toni Kangas <toni.ar.kangas@gmail.com>
2018-01-03 02:45:43 +0200

    Add mocked tests.

--- src/__tests__/LocationUtils.test.js
+++ src/__tests__/LocationUtils.test.js
@@ -6,4 +6,33 @@ describe('LocationUtils.getCoords',()=>{
 			expect(error_json.hasOwnProperty('error')).toBe(true);
 		});
 	});
+	it('rejects if get current position fails',()=>{
+		const mockGeolocation = {
+			getCurrentPosition: jest.fn((_,error)=>{
+				error();
+			})
+		};
+		const realGeoLocation = global.navigator.geolocation;
+		global.navigator.geolocation = mockGeolocation;
+
+		return LocationUtils.getCoords().catch((error_json)=>{
+			expect(error_json.hasOwnProperty('error')).toBe(true);
+			global.navigator.geolocation = realGeoLocation;
+		});
+	});
+	it('resolves when geolocation available',()=>{
+		const mockGeolocation = {
+			getCurrentPosition: jest.fn((success)=>{
+				success({coords: {latitude: 60, longitude: 24}});
+			})
+		};
+		const realGeoLocation = global.navigator.geolocation;
+		global.navigator.geolocation = mockGeolocation;
+
+		return LocationUtils.getCoords().then((location)=>{
+			expect(location.lat).toBe(60);
+			expect(location.lon).toBe(24);
+			global.navigator.geolocation = realGeoLocation;
+		});
+	});
 });
