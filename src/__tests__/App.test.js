4b9860ef8bc9e31256d8e789903659d365877099
Toni Kangas <toni.ar.kangas@gmail.com>
2018-01-03 02:46:23 +0200

    Add mounting tests.

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { mount, shallow } from 'enzyme';
 import App from '../App';
+import LocationUtils from '../LocationUtils';
 
 jest.useFakeTimers();
 
@@ -8,6 +9,33 @@ describe('App',()=>{
 	it('renders without crashing', () => {
 		mount(<App />);
 	});
+	it('has timers to update location and traffic data',()=>{
+		const locationSpy = jest.spyOn(App.prototype, 'updateCoords');
+		const querySpy = jest.spyOn(App.prototype, 'sendQueryAndUpdateState');
+
+		shallow(<App />);
+		jest.runOnlyPendingTimers();
+
+		expect(locationSpy).toHaveBeenCalled();
+		expect(querySpy).toHaveBeenCalled();
+
+		jest.resetAllMocks();
+		locationSpy.mockRestore();
+		querySpy.mockRestore();
+	});
+	it('polls location at mount time if available',()=>{
+		const coords = {lat: 60, lon: 24};
+		const coordsSpy = jest.spyOn(LocationUtils, 'getCoords').mockImplementation(()=>{
+			return Promise.resolve(coords);
+		});
+
+		shallow(<App />);
+		expect(coordsSpy).toHaveBeenCalled();
+		// TODO: Check that state is updated
+
+		jest.resetAllMocks();
+		coordsSpy.mockRestore();
+	}),
 	it('clears timers at unmount', () => {
 		const wrapper = shallow(<App />);
 		const ids = [
